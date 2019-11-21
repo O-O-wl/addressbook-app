de0a3ac43b9e4a5e1ee18180d7a1c5ba5f349820
부엉이 <o.o.wl.soft@gmail.com>
2019-11-22 04:47:00 +0900

    feat: tableViewDataSource 로직 구현

--- AddressBookApp/AddressBookApp/Common/Extensions/TableView+.swift
+++ AddressBookApp/AddressBookApp/Common/Extensions/TableView+.swift
@@ -6,4 +6,11 @@
 //  Copyright © 2019 이동영. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    
+    func dequeueReusableCell<Cell: Reuseable>(withType cellType: Cell.Type, for indexPath: IndexPath) -> Cell? {
+        return dequeueReusableCell(withIdentifier: Cell.reuseIdentifier, for: indexPath) as? Cell
+    }
+}
