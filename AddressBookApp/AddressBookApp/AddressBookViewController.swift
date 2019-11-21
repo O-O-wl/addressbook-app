42e310ff695059eaa45d69c5c8a3c5496c2f43df
부엉이 <o.o.wl.soft@gmail.com>
2019-11-22 04:47:00 +0900

    feat: tableViewDataSource 로직 구현

--- AddressBookApp/AddressBookApp/AddressBookViewController.swift
+++ AddressBookApp/AddressBookApp/AddressBookViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 protocol AddressBookViewType: AnyObject {
     
@@ -13,25 +15,20 @@ protocol AddressBookViewType: AnyObject {
 }
 
 class AddressBookViewController: UITableViewController, AddressBookViewType {
+    // MARK: - UI
+    
     // MARK: - Dependencies
     var viewModel: AddressBookViewModelType? {
         didSet { bindViewModel() }
     }
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
         setUpTableView()
     }
-
-    // MARK: - Table view data source
-
+}
 // MARK: - Bind
 extension AddressBookViewController {
     
@@ -58,68 +55,22 @@ extension AddressBookViewController {
 extension AddressBookViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return viewModel?.numOfAddress ?? 0
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard
+            let cell = tableView.dequeueReusableCell(withType: AddressCell.self, for: indexPath),
+            let address = viewModel?.getAddress(at: indexPath.row)
+            else { return AddressCell() }
+        
+        cell.configure(address)
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
