b6570a516d39f1d1494674e884f6502063268e51
부엉이 <o.o.wl.soft@gmail.com>
2019-11-22 04:41:03 +0900

    feat: ContactService  프레임워크에 접근하는 서비스 객체 구현

--- AddressBookApp/AddressBookApp/Constant/Contact.swift
+++ AddressBookApp/AddressBookApp/Constant/Contact.swift
@@ -5,5 +5,32 @@
 //  Created by 이동영 on 2019/11/22.
 //  Copyright © 2019 이동영. All rights reserved.
 //
-
+import Contacts
 import Foundation
+
+enum ContactKey: CaseIterable {
+    
+    case imageData
+    case givenName
+    case familyName
+    case emailAddresses
+    case phoneNumbers
+    
+    var descriptor: CNKeyDescriptor {
+        var keyString = ""
+        switch self {
+        case .imageData:
+            keyString = CNContactImageDataKey
+        case .givenName:
+            keyString = CNContactGivenNameKey
+        case .familyName:
+            keyString = CNContactFamilyNameKey
+        case .emailAddresses:
+            keyString = CNContactEmailAddressesKey
+        case .phoneNumbers:
+            keyString = CNContactPhoneNumbersKey
+        }
+        return keyString as CNKeyDescriptor
+    }
+}
+
--- AddressBookApp/AddressBookApp/Service/ContactService.swift
+++ AddressBookApp/AddressBookApp/Service/ContactService.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2019 이동영. All rights reserved.
 //
 
+import Contacts
 import Foundation
+
+
+class ContactService {
+   
+    // MARK: - Singtone
+    static let shared = ContactService()
+    
+    // MARK: - Properties
+    private let store = CNContactStore()
+    private let contactAccessQueue = DispatchQueue(label: "ContactAccessQueue",
+                                                   attributes: DispatchQueue.Attributes.concurrent)
+    // MARK: - Initializdr
+    private init() {}
+    
+    // MARK: - Methods
+    func fetchContacts(_ completion: @escaping ([CNContact]) -> Void) {
+        var result = [CNContact]()
+        let keysToFetch = ContactKey.allCases.map { $0.descriptor }
+        let request = CNContactFetchRequest(keysToFetch: keysToFetch)
+        
+        request.sortOrder = .familyName
+        
+        contactAccessQueue.async {
+            do {
+                try self.store.enumerateContacts(with: request) { contact, status in
+                    print(status)
+                    result.append(contact)
+                }
+                
+            } catch let error {
+                print("Error \(error.localizedDescription)")
+            }
+            
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+    }
+}
