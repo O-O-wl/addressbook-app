68e908d1bee117093bff2ec00ac7962915b8d812
부엉이 <o.o.wl.soft@gmail.com>
2019-11-22 04:21:24 +0900

    feat: 디렉터리 구성

---
+++ AddressBookApp/AddressBookApp/Common/Cell+Reuseable.swift
@@ -0,0 +1,15 @@
+//
+//  Reuseable.swift
+//  AddressBookApp
+//
+//  Created by 이동영 on 2019/11/22.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import Foundation
+
+protocol Reuseable {
+    
+    static var reuseIdentifier: String { get }
+}
+
---
+++ AddressBookApp/AddressBookApp/View/AddressCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddressCell.swift
+//  AddressBookApp
+//
+//  Created by 이동영 on 2019/11/22.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import UIKit
+
+class AddressCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
