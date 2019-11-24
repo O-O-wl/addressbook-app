d9ff88de9acbe6a397dc2912dc37f21978c12562
부엉이 <o.o.wl.soft@gmail.com>
2019-11-24 23:20:17 +0900

    reafactor: 구현이 필요없는 메소드 삭제

--- AddressBookApp/AddressBookApp/AppDelegate.swift
+++ AddressBookApp/AddressBookApp/AppDelegate.swift
@@ -17,19 +17,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         application.openSessions.first?.userInfo?["ViewModel"] = addressViewModel
         return true
     }
-    
-    // MARK: UISceneSession Lifecycle
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
