5fd4765d27515c94652d17b442daff2d0f3aff26
부엉이 <o.o.wl.soft@gmail.com>
2019-11-26 02:47:41 +0900

    refactor: 초성 분류라는 제약사항 추가에 따른 사용하는 자료구조 변경

--- AddressBookApp/AddressBookApp/ViewModel/AddressBookViewModel.swift
+++ AddressBookApp/AddressBookApp/ViewModel/AddressBookViewModel.swift
@@ -5,8 +5,10 @@
 //  Created by 이동영 on 2019/11/22.
 //  Copyright © 2019 이동영. All rights reserved.
 //
-
+import Contacts
 import Foundation
+
+typealias ContactsBundle = (initiality: String, list: [Address])
 
 // MARK: - AddressBookViewBindable
 protocol AddressBookViewBindable: AnyObject {
@@ -14,9 +16,10 @@ protocol AddressBookViewBindable: AnyObject {
     
     var dataDidLoad: (() -> Void)? { get set }
     var errorDidOccured: ((Error) -> Void)? { get set }
-    var count: Int { get }
+    var numOfBundles: Int { get }
     
-    subscript(at indexPath: IndexPath) -> Address? { get }
+    subscript(section: Int) -> ContactsBundle? { get }
+    subscript(row indexPath: IndexPath) -> Address? { get }
 }
 
 // MARK: - AddressBookViewModel
@@ -26,7 +29,7 @@ class AddressBookViewModel: AddressBookViewBindable {
     private let service: ContactService
     
     // MARK: - Properties
-    private var addresses: [Address]? {
+    private var addresses: [ContactsBundle]? {
         didSet { dataDidLoad?() }
     }
     
@@ -43,29 +46,43 @@ class AddressBookViewModel: AddressBookViewBindable {
     }
     
     // MARK: - Methods
-    var count: Int {
+    var numOfBundles: Int {
         return addresses?.count ?? 0
     }
     
-    subscript(at indexPath: IndexPath) -> Address? {
-        guard count > indexPath.row else { return nil }
-        
-        return addresses?[indexPath.row]
+    subscript(section: Int) -> ContactsBundle? {
+        return addresses?[section]
+    }
+    
+    subscript(row indexPath: IndexPath) -> Address? {
+        return addresses?[indexPath.section].list[indexPath.row]
     }
     
     private func fetchRequest() {
         service.fetchContacts { [weak self] result in
             switch(result) {
             case .success(let contacts):
-                self?.addresses = contacts.map {
-                    Address(imageData: $0.imageData,
-                            name: $0.givenName + $0.familyName,
-                            tel: $0.phoneNumbers.first?.value.stringValue ?? "",
-                            email: $0.emailAddresses.first?.value as String? ?? "" )
-                }
+                self?.addresses = contacts
+                    .compactMap { self?.parse(cotact: $0) }
+                    .reduce(into: [String: [Address]]()) { total, new in self?.classify(to: &total, with: new) }
+                    .map { (initiality: $0.key, list: $0.value) }
+                    .sorted { $0.initiality < $1.initiality }
             case .failure(let error):
                 self?.errorDidOccured?(error)
             }
+        }
+    }
+    
+    private func parse(cotact: CNContact) -> Address {
+        return  Address(imageData: cotact.imageData,
+                        name: cotact.familyName + cotact.givenName,
+                        tel: cotact.phoneNumbers.first?.value.stringValue ?? "",
+                        email: cotact.emailAddresses.first?.value as String? ?? "" )
+    }
+    
+    private func classify(to dictionary: inout [String: [Address]], with address: Address) {
+        if let firstSpell = address.name.firstSpell {
+            dictionary[firstSpell, default: [Address]()].append(address)
         }
     }
 }
