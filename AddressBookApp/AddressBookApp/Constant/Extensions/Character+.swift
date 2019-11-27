2686d46150ce0c5e896dcdee6433e0990110984a
부엉이 <o.o.wl.soft@gmail.com>
2019-11-27 18:26:55 +0900

    feat: 유니코드 정규화 추가 & String 익스텐션 코드 변경

--- AddressBookApp/AddressBookApp/Constant/Extensions/Character+.swift
+++ AddressBookApp/AddressBookApp/Constant/Extensions/Character+.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+extension Character {
+    
+    var initiality: Character? {
+        guard let scalar = "\(self)".normalize(.NFKD).unicodeScalars.first else { return nil }
+        
+        return Character(scalar)
+    }
+}
--- AddressBookApp/AddressBookApp/Constant/Extensions/String+.swift
+++ AddressBookApp/AddressBookApp/Constant/Extensions/String+.swift
@@ -10,15 +10,28 @@ import Foundation
 
 extension String {
     
+    var chacters: [Character] {
+        return self.map { $0 as Character }
+    }
+    
     var firstSpell: String? {
-        guard let scalar = self.unicodeScalars.first else { return nil }
-        
-        switch scalar.value {
-        case 0xAC00...0xD7A3:
-            let firstSpell = UnicodeScalar((scalar.value - 0xAC00) / 28 / 21 + 0x1100)
-            return firstSpell?.description
-        default:
-            return scalar.description
+        guard let firstSpell = self.first?.initiality else { return nil }
+        return String(firstSpell)
+    }
+    
+    func normalize(_ form: NormalizationForm) -> String {
+        switch form {
+        case .NFD: return self.decomposedStringWithCanonicalMapping
+        case .NFC: return self.precomposedStringWithCanonicalMapping
+        case .NFKD: return self.decomposedStringWithCompatibilityMapping
+        case .NFKC: return self.precomposedStringWithCompatibilityMapping
         }
+    }
+    
+    enum NormalizationForm {
+        case NFD
+        case NFC
+        case NFKD
+        case NFKC
     }
 }
